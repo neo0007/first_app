764020e6e369694888c39432d7296553c649fcb2
Neo <mr.yliu@gmail.com>
2017-04-06 22:38:32 +0800

    repaired all things

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  resources :topics
   root "topics#index"
+  resources :topics do
+    member do
+      post 'upvote'
+      post 'downvote'
+    end
+  end
+
+  get '/about', to:'topics#about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
